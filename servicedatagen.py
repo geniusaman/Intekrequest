d7c4b9178e66dedf20c5f69f97ac861e1aee5fdc
Aman Gupta <108857118+geniusaman@users.noreply.github.com>
2024-07-23 13:12:19 +0530

    Update servicedatagen.py

--- servicedatagen.py
+++ servicedatagen.py
@@ -170,7 +170,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-logo_base64 = get_base64_image("D:\\AI_team\\Sql_bot\\Easework logo.png")
+logo_base64 = get_base64_image("Easework logo.png")
 emoji_base64 = get_base64_image("emoji.png")
 
 # Add logo and style it
